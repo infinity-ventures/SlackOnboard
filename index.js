4b26660fa554c2ba580f33e8dced71dd78801e17
ensu <ensu.tw@gmail.com>
2020-07-22 10:47:28 +0800

    can update others profile by specifying their ID

--- index.js
+++ index.js
@@ -29,9 +29,10 @@ if (process.env.GOOGLE_KEY_JSON){
 }
 const bucketName = "ivs_attendees"
 
+const up_rex = /update\sprofile/i
+const user_id_rex = /\$([A-Z\d]+)\$/
 
 exports.profileFiller = async (req, res) => {
-
   let q = req.body;
 
   if (q.type === 'url_verification') {
@@ -45,28 +46,32 @@ exports.profileFiller = async (req, res) => {
   }
 
   // Events
-  let cond1 = q.event.type === "message" & q.event.text === "update profile";
+  let cond1 = q.event.type === "message" & q.event.text.match(up_rex) != null;
+  
   let cond2 = q.event.type === "team_join"
 
   if (cond1 | cond2) {
 
-
-    let user_id, email
+    let user_id, target_email, target_user_id
     if (cond1) {
-      user_id = q.event.user      
-      let resp = await web.users.profile.get({user: q.event.user, include_labels: true})
-      console.log(resp.profile)
-
-      email = resp.profile.email
-    } else {
-      user_id = q.event.user.id
-      email = q.event.user.profile.email
+      let extract_user_id = q.event.text.match(user_id_rex)
+      user_id = q.event.user 
+      if (extract_user_id) { // check if user is admin
+        let resp = await web.users.info({user: user_id})
+        if (!resp.user.is_admin) return noOpResponse()
+      }
+      target_user_id = extract_user_id != null? extract_user_id[1]: user_id
+      let resp = await web.users.profile.get({user: target_user_id, include_labels: true})  
+      target_email = resp.profile.email
+    } else {  // cond2
+      target_user_id = q.event.user.id
+      target_email = q.event.user.profile.email
     }
 
     // postgres client
     const client = new Client()
     await client.connect()
-    const result = await client.query(`SELECT * FROM ivs2020summer_attendees WHERE email='${email}';`)
+    const result = await client.query(`SELECT * FROM ivs2020summer_attendees WHERE email='${target_email}';`)
     client.end()
   
     if (result.rows.length === 0) return noOpResponse(res);  // Return if no related data
@@ -76,14 +81,17 @@ exports.profileFiller = async (req, res) => {
     // change profile info
     
     let custom_fields = {Xf017EPYEVHQ: {value: company_name}, 
-                         Xf017DD0FKS9: {value: title},
-                         Xf017F0QEZ9R: {value: category},
-                         Xf017M03U9UL: {value: industry},
-                         Xf017DCZF8SZ: {value: fb_url},
-                         Xf017DQLKDNZ: {value: username_jp}
+                        Xf017DD0FKS9: {value: title},
+                        Xf017F0QEZ9R: {value: category},
+                        Xf017M03U9UL: {value: industry},
+                        Xf017DCZF8SZ: {value: fb_url},
+                        Xf017DQLKDNZ: {value: username_jp}
     }
-    web.users.profile.set({user:user_id, profile:{"display_name":username, "real_name": username, "title": `${title} (${company_name} )`,
-                          fields: custom_fields}})
+
+    web.users.profile.set({user:target_user_id, profile:{"display_name":username, "real_name": username, "title": `${title} (${company_name} )`,
+                          fields: custom_fields}}).catch(e => {
+                            console.warn(e)
+                          })
 
     // list file names for debug
     /* const [files] = await storage.bucket(bucketName).getFiles();
@@ -96,13 +104,15 @@ exports.profileFiller = async (req, res) => {
     if (profie_img){
       storage.bucket(bucketName).file(profie_img).download(function(err, contents) {
         web.users.setPhoto({image: contents}) 
+      }).catch(e => {
+        console.warn(e)
       })
     }
- 
-    res.status(200);
-    res.json({});
-    return Promise.resolve();
   }
+
+  res.status(200);
+  res.json({});
+  return Promise.resolve();
 };
 function noOpResponse(response) {
   response.status(200).send('No flag emoji reaction detected.');
